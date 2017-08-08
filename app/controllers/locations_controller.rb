f4483c88e9b778f83dc1a8003adc982ffab03125
curiositypaths <curiositypaths@users.noreply.github.com>
2017-08-08 10:11:25 -0400

    Add location resource

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,19 @@
+class LocationsController < ApplicationController
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.create(location_params)
+    redirect_to @location
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name,:address)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/locations/new', to: 'locations#new'
+  post '/locations', to: 'locations#create', as: 'locations'
+  get '/location/:id', to: 'locations#show', as: 'location'
 end
